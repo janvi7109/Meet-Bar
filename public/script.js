0267a4ba2ebeb012d2fb2efb97e22667c5b294bf
janvi7109 <43574221+janvi7109@users.noreply.github.com>
2021-06-19 22:01:36 +0530

    changed

--- public/script.js
+++ public/script.js
@@ -1,6 +1,6 @@
 // const { PeerServer } = require("peer");
 console.log("Roo", ROOM_ID);
-const socket = io("/");
+// const socket = io("/");
 let myVideoStream;
 const videoGrid = document.getElementById("video-grid");
 const myVideo = document.createElement("video");
@@ -26,16 +26,16 @@ navigator.mediaDevices.getUserMedia({
     console.log(stream);
     myVideoStream = stream;
     addVideoStream(myVideo, stream);
-    peer.on("call", (call) => {
-        call.answer(stream);
-        const video = document.createElement("video");
-        call.on("stream", (userVideoStream) =>{
-            addVideoStream(video, userVideoStream);
-        });
-    });
-    socket.on("user-connected", (userId) => {
-        connectToNewUser(userId, stream);
-    });
+    // peer.on("call", (call) => {
+    //     call.answer(stream);
+    //     const video = document.createElement("video");
+    //     call.on("stream", (userVideoStream) =>{
+    //         addVideoStream(video, userVideoStream);
+    //     });
+    // });
+    // socket.on("user-connected", (userId) => {
+    //     connectToNewUser(userId, stream);
+    // });
 });
 
 const connectToNewUser = (userId, stream) => {
@@ -46,8 +46,8 @@ const connectToNewUser = (userId, stream) => {
     });
 };
 
-peer.on("open", (id) => {
-    socket.emit("join-room", ROOM_ID, id);
-});
+// peer.on("open", (id) => {
+//     socket.emit("join-room", ROOM_ID, id);
+// });
 
 
